feafef819dbd966f92eb2a6d076644e838d2e0d9
Joel Alejandre <joel@alejand.re>
2020-05-15 17:16:23 -0400

    corrected setup

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name="schema", version="0.1", packages=["src/schema"],
+    name="schema",
+    version="0.1",
+    package_dir={"": "src"},
+    packages=find_packages(where="schema"),
 )
